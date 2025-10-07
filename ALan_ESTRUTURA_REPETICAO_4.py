9ae1a7032700ba6bdc6caab0a85db2123e691b2e
alan-lab <alancatboy34@gmail.com>
2025-10-07 20:50:24 -0300

    Update ALan_ESTRUTURA_REPETICAO_4.py

--- ALan_ESTRUTURA_REPETICAO_4.py
+++ ALan_ESTRUTURA_REPETICAO_4.py
@@ -1,13 +1,21 @@
 # Lê o nome de usuário
 nome = input ("Digite seu nome: ")
 print(f"Olá, {nome}!")
-# Cria uma lista vaza para armazenar os números reais
-numeros = []
-# Lê 10 números reais do usuário
-for i in range(10):
-    num = float(input(f"Digite o número real {i + 1}: "))
-    numeros.append(num)
-# Mostra os números inteiros digitados
-print("Os números digitados em ordem inversa são:")
-for num in reversed(numeros):
-    print(num)
+# Cria uma lista para armazenar as notas 
+notas = []
+
+# lê 4 notas do usuário
+for i in range(4):
+    nota = float(input(f"Digite a {i + 1}a nota: "))
+    notas.append(nota)
+    
+#Calcula a média das notas
+media = sum(notas) / len(notas)
+
+# Mostra as notas
+print ("\n notss digitadas: ")
+for i, nota in enumerate(notas, start=1 ):
+    print(f"Nota {1}: {nota} ")
+    
+# Mostra a média
+print(f"\n Média das notas: {media: .2f} ")
